4cb24580078e0d9dc72faead6cc9fa141172edc9
Hiten <115025470+Hitenweb3@users.noreply.github.com>
2024-03-23 12:12:50 -0400

    working outline
    outline of frame

--- channelguide/src/index.tsx
+++ channelguide/src/index.tsx
@@ -1,57 +1,156 @@
 import { serveStatic } from '@hono/node-server/serve-static'
-import { Button, Frog, TextInput } from 'frog'
+import { Button, Frog } from 'frog'
 import { devtools } from 'frog/dev'
-// import { neynar } from 'frog/hubs'
+import { neynar } from 'frog/middlewares'
 
 type State = {
-  count: number
+  channels: string[],
+  data: string[],
+  casts: string[]
 }
  
 export const app = new Frog<{ State: State }>({
   initialState: {
-    count: 0
+    channels: ["Base", "Degen", "Founders"],
+    data: ["100", "200", "300"],
+    casts: ["yolo", "FC", "WAGMI"]
   }
 })
 
+// app.use(
+//   neynar({
+//     apiKey: 'C0E4D0E1-A9E4-40B7-B5CE-2CFA9FF4CE1F',
+//     features: ['interactor', 'cast'],
+//   }),
+// )
+
 app.use('/*', serveStatic({ root: './public' }))
- 
+
 app.frame('/', (c) => {
-  const { buttonValue, deriveState } = c
-  const state = deriveState(previousState => {
-    if (buttonValue === 'inc') previousState.count++
-    if (buttonValue === 'dec') previousState.count--
-  })
+  console.log('cast: ', c.var)
+  // const { displayName, followerCount } = c.var.interactor || {}
+  // console.log('cast: ', c.var.cast)
+  // console.log('interactor: ', c.var.interactor)
+
   return c.res({
     image: (
-      <div style={{ color: 'black', display: 'flex', fontSize: 60 }}>
-        Count: {state.count}
+      <div style={{ 
+        background: 'blue',
+        color: 'white', 
+        justifyContent: 'center', 
+        alignItems: 'center', 
+        fontSize: 60,
+        display: 'flex',
+        height: '100%',
+        textAlign: 'center',
+        width: '100%',
+        }}>
+        Channel Digest
+        {/* Greetings {displayName}, you have {followerCount} followers. */}
       </div>
     ),
     intents: [
-      <Button value="inc">Increment</Button>,
-      <Button value="dec">Decrement</Button>,
-      <Button action="/next" value="next">Next</Button>,
+      <Button action="/trendinglist" value="LetsGo">Lets Go</Button>,
     ]
   })
 })
 
-app.frame('/next', (c) => {
-  const {deriveState } = c
+
+// Frame to display casts 
+app.frame('/next', (c) => { 
+  return c.res({
+    image: (
+      <div style={{ color: 'black', display: 'flex', fontSize: 60 }}>
+        Casts:
+      </div>
+    ),
+    intents: [
+      <Button.Reset>Reset</Button.Reset>
+    ]
+  })
+})
+
+
+app.frame('/trendinglist', (c) => {
+  const { deriveState } = c
   const state = deriveState(previousState => {
-    previousState.count
+    previousState.channels
   })
 
   return c.res({
     image: (
       <div style={{ color: 'black', display: 'flex', fontSize: 60 }}>
-        Count: {state.count}
+       <ul>
+         {state.channels.map((value, index) => 
+            <li key={index}>{`${index + 1}. ${value}`}</li>
+            )}
+       </ul>
       </div>
     ),
     intents: [
-      <Button.Reset>Reset</Button.Reset>,
+      <Button action="/summary" value={state.channels[0]}>1</Button>,
+      <Button action="/summary" value={state.channels[1]}>2</Button>,
+      <Button action="/summary" value={state.channels[2]}>3</Button>,
+      <Button.Reset>Reset</Button.Reset>
     ]
   })
 })
+
+
+// Frame to display summmary 
+app.frame('/summary', (c) => { 
+  const { buttonValue } = c
+  return c.res({
+    image: (
+      <div style={{ color: 'black', display: 'flex', fontSize: 60 }}>
+        Summary: {buttonValue}
+      </div>
+    ),
+    intents: [
+      <Button action="/stats" value={buttonValue}>Stats</Button>,
+      <Button action="/recentcasts" value={buttonValue}>Casts</Button>,
+      <Button.Reset>Reset</Button.Reset>
+    ]
+  })
+})
+
+
+// Frame to display data 
+app.frame('/stats', (c) => { 
+  const { buttonValue } = c
+  return c.res({
+    image: (
+      <div style={{ color: 'black', display: 'flex', fontSize: 60 }}>
+        Stats: {buttonValue}
+      </div>
+    ),
+    intents: [
+      <Button action="/summary" value={buttonValue}>Summary</Button>,
+      <Button action="/recentcasts" value={buttonValue}>Casts</Button>,
+      <Button.Reset>Reset</Button.Reset>
+    ]
+  })
+})
+
+
+// Frame to display casts 
+app.frame('/recentcasts', (c) => { 
+  const { buttonValue } = c
+  return c.res({
+    image: (
+      <div style={{ color: 'black', display: 'flex', fontSize: 60 }}>
+        Casts: {buttonValue}
+      </div>
+    ),
+    intents: [
+      <Button action="/summary" value={buttonValue}>Summary</Button>,
+      <Button action="/stats" value={buttonValue}>Stats</Button>,
+      <Button.Reset>Reset</Button.Reset>
+    ]
+  })
+})
+
+
 
 devtools(app, { serveStatic })
 
