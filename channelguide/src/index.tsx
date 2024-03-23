5ca870a76c774bf86124058d1094d371670bf3a2
Hiten <115025470+Hitenweb3@users.noreply.github.com>
2024-03-23 14:20:48 -0400

    added neynar channel list
    added adding channel data

--- channelguide/src/index.tsx
+++ channelguide/src/index.tsx
@@ -1,13 +1,13 @@
 import { serveStatic } from '@hono/node-server/serve-static'
 import { Button, Frog } from 'frog'
 import { devtools } from 'frog/dev'
-import { neynar } from 'frog/middlewares'
 
 type State = {
   channels: string[],
   data: string[],
   casts: string[]
 }
+
  
 export const app = new Frog<{ State: State }>({
   initialState: {
@@ -17,21 +17,10 @@ export const app = new Frog<{ State: State }>({
   }
 })
 
-// app.use(
-//   neynar({
-//     apiKey: 'C0E4D0E1-A9E4-40B7-B5CE-2CFA9FF4CE1F',
-//     features: ['interactor', 'cast'],
-//   }),
-// )
-
 app.use('/*', serveStatic({ root: './public' }))
 
 app.frame('/', (c) => {
-  console.log('cast: ', c.var)
-  // const { displayName, followerCount } = c.var.interactor || {}
-  // console.log('cast: ', c.var.cast)
-  // console.log('interactor: ', c.var.interactor)
-
+  
   return c.res({
     image: (
       <div style={{ 
@@ -46,32 +35,24 @@ app.frame('/', (c) => {
         width: '100%',
         }}>
         Channel Digest
-        {/* Greetings {displayName}, you have {followerCount} followers. */}
       </div>
     ),
     intents: [
-      <Button action="/trendinglist" value="LetsGo">Lets Go</Button>,
+      <Button action="/trendingdata" value="LetsGo">Lets Go</Button>,
     ]
   })
 })
 
-
-// Frame to display casts 
-app.frame('/next', (c) => { 
-  return c.res({
-    image: (
-      <div style={{ color: 'black', display: 'flex', fontSize: 60 }}>
-        Casts:
-      </div>
-    ),
-    intents: [
-      <Button.Reset>Reset</Button.Reset>
-    ]
+app.frame('/trendingdata', async (c) => {
+  const response = await fetch('https://api.neynar.com/v2/farcaster/channel/trending?time_window=7d', {
+    method: 'GET',
+    headers: {
+      'accept': 'application/json',
+      'api_key': 'C0E4D0E1-A9E4-40B7-B5CE-2CFA9FF4CE1F'
+    }
   })
-})
-
-
-app.frame('/trendinglist', (c) => {
+  const channelistres = await response.json()
+  console.log(channelistres)
   const { deriveState } = c
   const state = deriveState(previousState => {
     previousState.channels
@@ -79,12 +60,11 @@ app.frame('/trendinglist', (c) => {
 
   return c.res({
     image: (
-      <div style={{ color: 'black', display: 'flex', fontSize: 60 }}>
-       <ul>
-         {state.channels.map((value, index) => 
-            <li key={index}>{`${index + 1}. ${value}`}</li>
-            )}
-       </ul>
+      <div style={{ color: 'black', display: 'flex', flexDirection: 'column', fontSize: 40 }}>
+        <h2>Trending Channels</h2>
+        {channelistres.channels.slice(0, 5).map((item, index) => (
+          <div key={index}>{`${index + 1}. ${JSON.stringify(item.channel.name)}`}</div>  
+        ))}
       </div>
     ),
     intents: [
@@ -95,6 +75,7 @@ app.frame('/trendinglist', (c) => {
     ]
   })
 })
+
 
 
 // Frame to display summmary 
@@ -155,6 +136,84 @@ app.frame('/recentcasts', (c) => {
 devtools(app, { serveStatic })
 
 
+// app.frame('/trendinglist', neynarMiddleware, (c) => {
+//   console.log('interactor: ', c.var)
+//   const { deriveState } = c
+//   const state = deriveState(previousState => {
+//     previousState.channels
+//   })
+
+//   return c.res({
+//     image: (
+//       <div style={{ color: 'black', display: 'flex', fontSize: 60 }}>
+//        <ul>
+//          {state.channels.map((value, index) => 
+//             <li key={index}>{`${index + 1}. ${value}`}</li>
+//             )}
+//        </ul>
+//       </div>
+//     ),
+//     intents: [
+//       <Button action="/summary" value={state.channels[0]}>1</Button>,
+//       <Button action="/summary" value={state.channels[1]}>2</Button>,
+//       <Button action="/summary" value={state.channels[2]}>3</Button>,
+//       <Button.Reset>Reset</Button.Reset>
+//     ]
+//   })
+// })
+
+
+// app.frame('/trendingdata', async (c) => {
+//   const response = await fetch('https://api.neynar.com/v2/farcaster/feed/trending?limit=10&time_window=24h', {
+//     method: 'GET',
+//     headers: {
+//       'accept': 'application/json',
+//       'api_key': 'C0E4D0E1-A9E4-40B7-B5CE-2CFA9FF4CE1F'
+//     }
+//   })
+//   const data = await response.json()
+//   console.log(data)
+//   return c.res({
+//     image: (
+//       <div style={{ color: 'black', display: 'flex', flexDirection: 'column', fontSize: 20 }}>
+//         {data.casts.map((item, index) => (
+//           <div key={index}>{JSON.stringify(item.text)}</div>
+//         ))}
+//       </div>
+//     ),
+//     intents: [
+//       <Button.Reset>Reset</Button.Reset>
+//     ]
+//   })
+// })
+
+
+
+// app.frame('/trendinglist', neynarMiddleware, (c) => {
+//   console.log('interactor: ', c.var)
+//   const { deriveState } = c
+//   const state = deriveState(previousState => {
+//     previousState.channels
+//   })
+
+//   return c.res({
+//     image: (
+//       <div style={{ color: 'black', display: 'flex', fontSize: 60 }}>
+//        <ul>
+//          {state.channels.map((value, index) => 
+//             <li key={index}>{`${index + 1}. ${value}`}</li>
+//             )}
+//        </ul>
+//       </div>
+//     ),
+//     intents: [
+//       <Button action="/summary" value={state.channels[0]}>1</Button>,
+//       <Button action="/summary" value={state.channels[1]}>2</Button>,
+//       <Button action="/summary" value={state.channels[2]}>3</Button>,
+//       <Button.Reset>Reset</Button.Reset>
+//     ]
+//   })
+// })
 
 
 //Template
